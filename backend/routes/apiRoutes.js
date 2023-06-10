d7232fd1dc37860dbe039d5fc7e390af0f8bfe67
Natalia Pirath <natpirath@gmail.com>
2023-06-10 11:23:14 -0400

    I added the necessary changes for the table to work. This version is before adding the other buttons.

--- backend/routes/apiRoutes.js
+++ backend/routes/apiRoutes.js
@@ -2,28 +2,7 @@ const express = require('express');
 const router = express.Router();
 const recordsController = require('../controllers/recordsController');
 
-// GET all records
+// Route for getting records
 router.get('/records', recordsController.getRecords);
-
-// GET a single record by ID
-router.get('/records/:id', recordsController.displayRecord);
-
-// GET multiple records within a range
-router.get('/records', recordsController.displayRecords);
-
-// POST create a new record
-router.post('/records', recordsController.createRecord);
-
-// PUT edit an existing record
-router.put('/records/:id', recordsController.editRecord);
-
-// DELETE delete a record
-router.delete('/records/:id', recordsController.deleteRecord);
-
-// POST reload the data from the dataset
-router.post('/records/reload', recordsController.reloadRecords);
-
-// POST save records to file
-router.post('/records/save', recordsController.saveRecords);
 
 module.exports = router;
