628ed6bd111b5ece642a32839f048bec4e9e64bf
Natalia Pirath <natpirath@gmail.com>
2023-07-18 22:38:59 -0400

    updated sorting functionaloty to use a BinarySearchTree

--- backend/services/binarySearchTree.js
+++ backend/services/binarySearchTree.js
@@ -1,6 +1,7 @@
 class Node {
-    constructor(data) {
-      this.data = data;
+    constructor(value, record) {
+      this.value = value;
+      this.records = [record];
       this.left = null;
       this.right = null;
     }
@@ -11,10 +12,9 @@ class Node {
       this.root = null;
     }
   
-    insert(data) {
-      const newNode = new Node(data);
-  
-      if (!this.root) {
+    insert(value, record) {
+      const newNode = new Node(value, record);
+      if (this.root === null) {
         this.root = newNode;
       } else {
         this.insertNode(this.root, newNode);
@@ -22,33 +22,32 @@ class Node {
     }
   
     insertNode(node, newNode) {
-      if (newNode.data.GEO < node.data.GEO) {
-        if (!node.left) {
+      if (newNode.value < node.value) {
+        if (node.left === null) {
           node.left = newNode;
         } else {
           this.insertNode(node.left, newNode);
         }
-      } else {
-        if (!node.right) {
+      } else if (newNode.value > node.value) {
+        if (node.right === null) {
           node.right = newNode;
         } else {
           this.insertNode(node.right, newNode);
         }
+      } else {
+        node.records.push(newNode.records[0]); // Push the new record into the existing node's records array
       }
     }
   
-    inOrderTraversal(callback) {
-      this.inOrderTraversalNode(this.root, callback);
-    }
-  
-    inOrderTraversalNode(node, callback) {
-      if (node) {
-        this.inOrderTraversalNode(node.left, callback);
-        callback(node.data);
-        this.inOrderTraversalNode(node.right, callback);
+    inOrder(node, callback) {
+      if (node !== null) {
+        this.inOrder(node.left, callback);
+        for (const record of node.records) {
+          callback(record); // Call the callback function for each record
+        }
+        this.inOrder(node.right, callback);
       }
     }
   }
   
   module.exports = BinarySearchTree;
-  
